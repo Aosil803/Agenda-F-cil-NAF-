85f244da8e80f01ff6abf65d6583119b738ef752
TatiClaussen <TATICLAUSSEN@YAHOO.COM.BR>
2024-11-24 14:52:44 -0300

    Merge branch 'main' of https://github.com/Aosil803/AgendaFacil_NAF_Unifeso

---
+++ back_end/dtos/agendamento.py
@@ -0,0 +1,16 @@
+# from pydantic import BaseModel
+# from datetime import datetime
+
+# class AgendamentoBase(BaseModel):
+#     usuario_id: int
+#     horario_disponivel_id: int
+
+# class AgendamentoCriar(AgendamentoBase):
+#     pass
+
+# class Agendamento(AgendamentoBase):
+#     id: int
+#     data_agendamento: datetime
+
+#     class Config:
+#         orm_mode = True
